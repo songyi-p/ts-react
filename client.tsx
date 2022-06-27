18f0a298da1e4873d52fb0e81ce46f5911db9021
songyi-p <songyi7091@gmail.com>
2022-06-27 23:42:15 +0900

    feat: section1 숫자야구 게임 (학습 사항 : props 타입 지정)

--- client.tsx
+++ client.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import WordRelay from './WordRelay';
+import NumberBaseball from './section1/NumberBaseball'
 
-ReactDOM.render(<WordRelay />, document.querySelector('#root'));
+ReactDOM.render(<NumberBaseball />, document.querySelector('#root'));
